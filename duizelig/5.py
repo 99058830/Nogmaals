b90f42ebcbd76a9daf81e3c5c5e52a085b479db0
99058830 <99058830@mydavinci.nl>
2021-11-02 11:25:44 +0100

    1 t/m pushed

---
+++ duizelig/5.py
@@ -0,0 +1,8 @@
+i = 1
+while True:
+    putput = input('Hey, wat doe jij hier >>> ')
+    print('Ik blijf herhalen:',i,' ')
+    if putput != 'quit':
+        i+=1
+    else:
+        exit()
---
+++ duizelig/6.py
@@ -0,0 +1,6 @@
+x = 50
+i = 0
+while i <= 1000:
+    i += x
+    print(i)
+    x += 1
