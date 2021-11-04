263831355e65650f1f6aa0663e5a222a6df1249d
99058830 <99058830@mydavinci.nl>
2021-11-04 11:42:21 +0100

    Added printed list

--- coins.py
+++ coins.py
@@ -7,6 +7,7 @@
 toPay = int(float(input('Amount to pay: '))* 100) #Input voor het te betalen bedrag
 paid = int(float(input('Paid amount: ')) * 100) #Input voor het al eerder betaalde bedrag
 change = paid - toPay #Rekensom om wisselgeld terug te geven
+changeList = []
 
 if change > 0: #Checked of er uberhaupt wisselgeld nodig is
   coinValue = 500 #Value moet hoger omdat ik een munt stuk van 5,00 toe moest voegen
@@ -17,6 +18,7 @@ if change > 0: #Checked of er uberhaupt wisselgeld nodig is
     if nrCoins > 0: #
       print('return maximal ', nrCoins, ' coins of ', coinValue, ' cents!' ) #Print om de uitkomst uit te printen
       nrCoinsReturned = int(input('How many coins of ' + str(coinValue) +  ' cents did you return? ')) #Om uit te printen wat je terug krijgt
+      changeList.append(str(nrCoins)+' coins of €'+str(format(float(coinValue/100),'.2f')))
       change -= nrCoinsReturned * coinValue #Rekensom voor change
 
 # comment on code below: Als het 5 euro is krijg je 3 euro terug, als je 3 euro hebt krijg je 2 euro terug enz.
@@ -24,7 +26,7 @@ if change > 0: #Checked of er uberhaupt wisselgeld nodig is
       coinValue = 300
     elif coinValue == 300:
       coinValue = 200
-    elif coinValue == 200
+    elif coinValue == 200:
       coinValue = 50
     elif coinValue == 50:
       coinValue = 20
@@ -39,7 +41,10 @@ if change > 0: #Checked of er uberhaupt wisselgeld nodig is
     else:
       coinValue = 0
 
-if change > 0: #Als het kleiner is dan hoeft je het niet te printen
+if change > 0: #Als het 0 is kan het niet terug betaald worden, anders moet je printen
   print('Change not returned: ', str(change) + ' cents')
 else:
-  print('done')
+  print('Here is your payout : ')
+  x = 0
+  for x in range(len(changeList)):
+    print(str(changeList[x]))
